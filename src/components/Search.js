84a7f5542095d4503e514af8c7b21d84bd2949e1
Zack90305 <zwashdev@gmail.com>
2021-01-06 10:36:18 +0000

    Learning the importance of debounce in vid 169 of React tutorial. Prevents inefficient network requests and uses each component of state (term, debouncedTerm, results) to prevent hard errors

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,9 +2,22 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-	const [term, setTerm] = useState('');
+	const [term, setTerm] = useState('programming');
+	const [debouncedTerm, setDebouncedTerm] = useState(term);
 	const [results, setResults] = useState([]);
 	
+	//debounce causes the timer for the network request to update term to reset. This staggers the network request for term
+	useEffect(() => {
+		const timerId = setTimeout(() => {
+			setDebouncedTerm(term);
+		}, 1000);
+		
+		return () => {
+			clearTimeout(timerId);
+		};
+	}, [term]);
+	
+	//only runs on page render or when debounce term changes
 	useEffect(() => {
 		const search = async () => {
 			const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
@@ -13,30 +26,13 @@ const Search = () => {
 					list: 'search',
 					origin: '*',
 					format: 'json',
-					srsearch: term
+					srsearch: debouncedTerm
 				},
-			});
-			
+			});	
 			setResults(data.query.search);
 		};
-		
-		if (term && !results.length){
-			search();
-		} else {
-			const timeoutId = setTimeout(() =>{
-				if (term){
-					search();
-				}
-			}, 750);
-			
-			return () => {
-				clearTimeout(timeoutId);
-			};
-		}
-		
-		
-		
-	}, [term]);
+		search();
+	}, [debouncedTerm])
 	
 	const renderedResults = results.map((result) => {
 		return (

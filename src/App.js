b935dd7a37e320a0f390c955bbfc2768c68ad8c2
Zack90305 <zwashdev@gmail.com>
2021-01-20 06:26:16 +0000

    Completed language conversion widget

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
 
 const items = [
 	{
@@ -35,19 +36,22 @@ const options = [
 ];
 
 export default () => {
-	const [selected, setSelected] = useState(options[0]);
-	const [showDropdown, setShowDropdown] = useState(true);
+	{/*const [selected, setSelected] = useState(options[0]);*/}
+	{/*const [showDropdown, setShowDropdown] = useState(true);*/}
 	
-	return (
+	return ( 
+	
 		<div>
+			<Translate />
 			{/* <Accordion items={items} /> */}
 			{/* <Search /> */}
-			<button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
-			{showDropdown ?
+			{/*<button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>*/}
+			{/*{showDropdown ?
 				<Dropdown options={options} selected={selected} onSelectedChange={setSelected}/> : null
-			}
+			}*/}
 			
 		</div>
+		
 	);
 };
 
